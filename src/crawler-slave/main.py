f831cc1104c41679c5ae39e123abf7964028f221
archer <1246506786@qq.com>
2015-09-16 02:51:23 -0400

    gae flask used

--- src/crawler-slave/main.py
+++ src/crawler-slave/main.py
@@ -1,17 +1,39 @@
 """`main` is the top level module for your Flask application."""
-
+import sys
+sys.path.insert(0, 'lib')
 # Import the Flask Framework
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
-
+from google.appengine.api import urlfetch
+import validators
 
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
     return 'Hello World!'
 
+@app.route('/url')
+def get_data_by_url():
+    url = request.args.get('url', '')
+
+    data = {'err': False, 'msg': None, 'status_code': None, 'url': url, 'data': None}
+
+    if not validators.url(url):
+        data['err'] = True
+        data['msg'] = 'invalid url'
+        return jsonify(data)
+
+    try:
+        result = urlfetch.fetch(url)
+        data['status_code'] = result.status_code
+        data['data'] = result.content
+    except:
+        data['err'] = True
+        data['msg'] = 'GAE Download Error'
+
+    return jsonify(data)
 
 @app.errorhandler(404)
 def page_not_found(e):
