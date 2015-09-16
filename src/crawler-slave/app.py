5a6483065db85ed9b0d14dfafbcb6340db8de0cd
archer <1246506786@qq.com>
2015-09-15 23:59:26 -0400

    can use third party lib

--- src/crawler-slave/app.py
+++ src/crawler-slave/app.py
@@ -1,12 +1,29 @@
+import sys
+sys.path.insert(0, 'lib')
 import webapp2
+from google.appengine.api import urlfetch
 import json
+import validators
 
-class MainPage(webapp2.RequestHandler):
+class Crawler(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'application/json'
-        data = {'name': 'archer', 'age': 22, 'err': False}
+
+        url = self.request.get('url')
+
+        data = {'status_code': None, 'url': url, 'err': False, 'msg': None, 'data': None}
+        
+        if not validators.url(url):
+            data['err'] = True
+            data['msg'] = 'invalid url'
+            self.response.write(json.dumps(data))
+            return
+
+        result = urlfetch.fetch(url)
+        data['status_code'] = result.status_code
+        data['data'] = result.content
         self.response.write(json.dumps(data))
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/url', Crawler),
 ], debug=True)
