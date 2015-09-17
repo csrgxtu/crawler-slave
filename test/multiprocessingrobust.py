dd773a4445a6c009747c8ec4da320bff3776ede2
archer <1246506786@qq.com>
2015-09-17 06:34:23 -0400

    multiprocessing not working right now

--- test/multiprocessingrobust.py
+++ test/multiprocessingrobust.py
@@ -17,7 +17,7 @@ def worker(appids, isbns):
     appidsCycle = cycle(appids)
 
     for isbn in isbns:
-        url = 'http://' + appidsCycle.next() + '.appspot.com/url?url=' + 'http://book.douban.com/isbn/' + isbn
+        url = 'http://' + str(appidsCycle.next()) + '.appspot.com/url?url=' + 'http://book.douban.com/isbn/' + str(isbn)
         print 'DEBUG: ', url
 
         d = Download(url)
