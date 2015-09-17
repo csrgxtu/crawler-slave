623839854ece006c1fda36d1218e3476ad266ff1
archer <1246506786@qq.com>
2015-09-16 21:14:44 -0400

    crawlerapitester.py ready

--- test/crawlerapitester.py
+++ test/crawlerapitester.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
 #
+# Usage: python crawlerapitester.py 10
 #
 from Download import Download
 import json
+import sys
 
-url = '
+url = 'http://csrgxtu01.appspot.com/url?url=http://book.douban.com/isbn/9787508653594'
+
+for i in range(int(sys.argv[1])):
+    d = Download(url)
+    if d.doRequest():
+        print i, 'cant doRequest'
+        continue
+
+    j = json.loads(d.getSOURCE())
+    print i, j['err'], j['status_code']
