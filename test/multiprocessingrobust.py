2c79e2d904393a795027f0921a1793c9c7cbf8e0
archer <1246506786@qq.com>
2015-09-17 06:24:03 -0400

    multiprocessing finished, not teste dyet

--- test/multiprocessingrobust.py
+++ test/multiprocessingrobust.py
@@ -8,14 +8,34 @@
 #
 # Produced By BR(BeautifulReading)
 import multiprocessing
+from util import loadIsbns
+from itertools import cycle
+import json
+from Download import Download
 
-def worker(appid, isbn):
-    print 'Worker'
+def worker(appids, isbns):
+    appidsCycle = cycle(appids)
+
+    for isbn in isbns:
+        url = 'http://' + appidsCycle.next() + '.appspot.com/url?url=' + 'http://book.douban.com/isbn' + isbn
+        print 'DEBUG: ', url
+
+        d = Download(url)
+        if d.doRequest():
+            print isbn, 'network error'
+            continue
+
+        j = json.loads(d.getSOURCE())
+        print isbn, j['status_code']
+
     return
 
 if __name__ == '__main__':
+    isbns = loadIsbns('isbns.txt')
+    appids = loadIsbns('appids')
+
     jobs = []
     for i in range(10):
-        p = multiprocessing.Process(target=worker,args = (appid, isbn))
+        p = multiprocessing.Process(target=worker,args = (appids[(800 * i):(i * 800 + 800)], isbns))
         jobs.append(p)
         p.start()
